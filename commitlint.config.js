92ff331794cf3b36a02e201b94c76259c3e72fd9
Jiaxing Shi <jiaxshi@qti.qualcomm.com>
2025-05-19 17:37:23 +0800

    fix: add "release" type
    Signed-off-by: Jiaxing Shi <jiaxshi@qti.qualcomm.com>

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,22 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     "body-case": [2,"always", "sentence-case"],
-    "body-leading-blank": [2, "always"]
+    "body-leading-blank": [2, "always"],
+    "type-enum": [2, "always",
+        [
+            "build",
+            "chore",
+            "ci",
+            "docs",
+            "feat",
+            "fix",
+            "perf",
+            "refactor",
+            "revert",
+            "style",
+            "test",
+            "release",
+        ]
+    ],
   }
 };
